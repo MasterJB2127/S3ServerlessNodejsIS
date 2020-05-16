3dbeda6774467433567ee506a556d39100f74da5
Josue <bonillamjosuei@outlook.com>
2020-05-15 18:44:39 -0600

    V1

---
+++ Tests/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var savefile = require('./functions/saveFile');
+var get = require('./functions/getFile');
+
+//console.log(get.getFile('C1.pdf'));
+
+
+savefile.saveFile("./Bandera.jpg", 'Bandera2.jpg');
